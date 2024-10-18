8d36fa32e9bf3cf22846971a965385dd9e64c112
Brian <brianhilsden@gmail.com>
2024-10-18 20:44:45 +0300

    updated featured projects on landing page

--- src/app/page.js
+++ src/app/page.js
@@ -129,14 +129,12 @@ export default function Home() {
               className="bg-gray-800 rounded-lg shadow-lg p-6 hover:bg-gray-700 transition"
               whileHover={{ scale: 1.05 }}
             >
-              <h3 className="text-xl font-semibold mb-4">Royalty Trading</h3>
+              <h3 className="text-xl font-semibold mb-4">MunchInKenya</h3>
               <p className="text-gray-300">
-                Royalty Trading is a dynamic e-commerce platform built with
-                vanilla JavaScript, designed to provide seamless buying and
-                selling experiences.
+              MunchInKenya is a dynamic single-page food delivery application designed to connect users with a diverse range of Kenyan restaurants and culinary offerings.
               </p>
               <Link
-                href="https://brianhilsden.github.io/Royalty-trading/"
+                href="https://brianhilsden.github.io/MunchInKenya-fe/"
                 target="_blank"
                 className="text-indigo-500 mt-4 inline-block"
               >

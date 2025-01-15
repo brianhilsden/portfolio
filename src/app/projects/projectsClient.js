7eed7aab58066b734a617f7f7b529dd4456f51c2
Brian <brianhilsden@gmail.com>
2025-01-15 14:00:13 +0300

    updated portfolio projects

--- src/app/projects/projectsClient.js
+++ src/app/projects/projectsClient.js
@@ -13,12 +13,20 @@ const projects = [
  
   },
   {
-    title: "Learning Management System",
+    title: "ITIK Solutions: Company Website Project",
     description:
-      "This Django REST API is designed to power a robust Learning Management System, providing endpoints for managing courses, user registrations, enrollments, and role-based access control. Instructors, students, and administrators can seamlessly interact with the system, leveraging various access levels to perform actions like course creation, assignment submission, and grading.",
-    imageUrl: "/images/LMS.png",
-    link: "https://learning-management-system-alpha-eosin.vercel.app/docs/",
-    tech_stack: "Django REST Framework, PostgreSQL"
+      "ITIK Solutions specializes in providing tech solutions with a focus on delivering fast, responsive, and visually engaging user experiences. The website showcases the company's services, mentorship opportunities, and contact information, utilizing modern technologies to ensure an impactful online presence.",
+    imageUrl: "/images/itik.png",
+    link: "https://www.itiksolutions.com/",
+    tech_stack: "Vite, React, Tailwind CSS, Framer Motion"
+  },
+  {
+    title: "KejaRMS: Rental Management System",
+    description:
+      "KejaRMS from TechVoyage is a comprehensive rental management system designed to streamline property management for landlords and tenants. It includes features like automated invoicing, tenant communication, property listings, and a tenant dashboard for managing requests and payments.",
+    imageUrl: "/images/keja.png",
+    link: "https://info.keja-rms.co.ke/",
+    tech_stack: "React, TypeScript, SpringBoot, PostgreSQL, Firebase"
   },
   {
     title: "TechVoyage: Food Retail Management Dashboard Project",
@@ -26,8 +34,19 @@ const projects = [
       "The Retail Management App from TechVoyage is a comprehensive web application designed for Admins, Salespeople, Riders, and Business Owners. Featuring both admin and client sides, this powerful tool allows users to manage orders, oversee transactions, and optimize inventory and deliveries, ensuring smooth operations.",
     imageUrl: "/images/RetailApp.png",
     link: "https://retail-app-kappa.vercel.app/",
-    tech_stack: "React.js, Spring Boot"
+    tech_stack: "React.js"
   },
+
+  {
+    title: "Learning Management System",
+    description:
+      "This Django REST API is designed to power a robust Learning Management System, providing endpoints for managing courses, user registrations, enrollments, and role-based access control. Instructors, students, and administrators can seamlessly interact with the system, leveraging various access levels to perform actions like course creation, assignment submission, and grading.",
+    imageUrl: "/images/LMS.png",
+    link: "https://learning-management-system-alpha-eosin.vercel.app/docs/",
+    tech_stack: "Django REST Framework, PostgreSQL"
+  },
+
+ 
 
 ];
 

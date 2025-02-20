022699f148afd4e9694a6618ed6363ac4cc72f2f
Brian <brianhilsden@gmail.com>
2025-02-20 10:12:29 +0300

    updates

--- src/app/projects/projectsClient.js
+++ src/app/projects/projectsClient.js
@@ -3,35 +3,20 @@ import { motion } from "framer-motion";
 import ProjectCard from "../components/common/ProjectsCard";
 
 const projects = [
-  {
-    title: "TechVoyage: Company Website Project",
-    description:
-      "TechVoyage specializes in delivering innovative web solutions. The company focuses on ensuring seamless user experiences. We utilize the latest technologies to meet your unique needs and take pride in crafting solutions that align with your vision and drive measurable results.",
-    imageUrl: "/images/TechVoyageCompany.png",
-    link: "https://techvoyage.co.ke",
-    tech_stack: "Next.js, Node.js, Express.js, PostgreSQL"
- 
-  },
-  {
-    title: "ITIK Solutions: Company Website Project",
-    description:
-      "ITIK Solutions specializes in providing tech solutions with a focus on delivering fast, responsive, and visually engaging user experiences. The website showcases the company's services, mentorship opportunities, and contact information, utilizing modern technologies to ensure an impactful online presence.",
-    imageUrl: "/images/itik.png",
-    link: "https://www.itiksolutions.com/",
-    tech_stack: "Vite, React, Tailwind CSS, Framer Motion"
-  },
+  
+
   {
     title: "KejaRMS: Rental Management System",
     description:
-      "KejaRMS from TechVoyage is a comprehensive rental management system designed to streamline property management for landlords and tenants. It includes features like automated invoicing, tenant communication, property listings, and a tenant dashboard for managing requests and payments.",
+      "KejaRMS is a comprehensive rental management system designed to streamline property management for landlords and tenants. It includes features like automated invoicing, tenant communication, property listings, and a tenant dashboard for managing requests and payments.",
     imageUrl: "/images/keja.png",
     link: "https://info.keja-rms.co.ke/",
     tech_stack: "React, TypeScript, SpringBoot, PostgreSQL, Firebase"
   },
   {
-    title: "TechVoyage: Food Retail Management Dashboard Project",
+    title: "Food Retail Management",
     description:
-      "The Retail Management App from TechVoyage is a comprehensive web application designed for Admins, Salespeople, Riders, and Business Owners. Featuring both admin and client sides, this powerful tool allows users to manage orders, oversee transactions, and optimize inventory and deliveries, ensuring smooth operations.",
+      "The Retail Management App is a comprehensive web application designed for Admins, Salespeople, Riders, and Business Owners. Featuring both admin and client sides, this powerful tool allows users to manage orders, oversee transactions, and optimize inventory and deliveries, ensuring smooth operations.",
     imageUrl: "/images/RetailApp.png",
     link: "https://retail-app-kappa.vercel.app/",
     tech_stack: "React.js"
@@ -45,9 +30,6 @@ const projects = [
     link: "https://learning-management-system-alpha-eosin.vercel.app/docs/",
     tech_stack: "Django REST Framework, PostgreSQL"
   },
-
- 
-
 ];
 
 
